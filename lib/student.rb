b820baafd917fecd3e887942e47286908ad28bb2
Judy McGoldrick <jjeanius@verizon.net>
2018-05-03 00:51:52 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -39,7 +39,7 @@ class Student
 
       DB[:conn].execute(sql, self.name, self.grade)
 
-      @id = DB[:conn].execute("SELECT id FROM students")
+      @id = DB[:conn].execute("SELECT id FROM students")=0
 
     end
 
