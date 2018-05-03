8b9f316c5215be9678a652423e95c83c68125e23
Judy McGoldrick <jjeanius@verizon.net>
2018-05-03 00:36:17 +0000

    automatically backed up by learn

--- lib/student.rb
+++ lib/student.rb
@@ -37,9 +37,9 @@ class Student
         VALUES (?, ?)
       SQL
 
-      DB[:conn].execute(sql, self.name, self.grade)
+      @id = DB[:conn].execute(sql, self.name, self.grade)
 
-      @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
+    #  @id = DB[:conn].execute("SELECT last_insert_rowid() FROM students")
 
     end
 
